17962b525f998cf527f8680db7ff4badf79f858e
Laucher Charlette <lcharlet@mi-a3.msk.21-school.ru>
2022-04-24 17:42:56 +0300

    ex02

--- ex01/test.cpp
+++ ex01/test.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <iomanip>
 
-int main()
+void test_1()
+{
+	int a = 1; //00000111
+	std::cout << a << '\n';
+	a = a << 1;
+	std::cout << a << '\n';
+	a = 7;
+	a = a << 2;
+	// std::cout << a << '\n';
+	a = a >> 2;
+	// std::cout << a << '\n';
+}
+
+void test_2()
 {
 	float a = 5.0f;
 	float b = 5.0;
@@ -16,5 +29,17 @@ int main()
     std::cout << 0.0000987654321f << '\n';
 	std::cout << std::setprecision(16); // show 16 digits of precision
     std::cout << 3.33333333333333333333333333333333333333f <<'\n'; // f suffix means float
+}
 
+void test_3()
+{
+	int x = 10;
+	x = x << 8;
+	std::cout << x << '\n';
+}
+
+int main()
+{
+	test_1();
+	return (0);
 }
